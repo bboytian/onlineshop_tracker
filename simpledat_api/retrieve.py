54eab8c217b97b86ad8c46ebcd4da87a3d1968c3
bboytian <94tian@gmail.com>
2020-08-15 22:11:44 +0800

    first commit

---
+++ simpledat_api/retrieve.py
@@ -0,0 +1,13 @@
+# imports
+from ..global_imports import *
+
+
+# main func
+def main(args):
+    # define your function here
+    pass
+
+
+# testing
+if __name__ == '__main__':
+    main()
---
+++ simpledat_api/update.py
@@ -0,0 +1,15 @@
+# imports
+from .retrieve import main as retrieve
+from .store import main as store
+from ..global_imports import *
+
+
+# main func
+def main(args):
+    # define your function here
+    pass
+
+
+# testing
+if __name__ == '__main__':
+    main()
